3f13f093e8317a38f7e5cfd479034bd9bb5c2653
Daniel McKemie <danielmckemie@Daniels-MacBook-Pro.local>
2019-05-31 14:59:15 -0400

    second draft

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -48,11 +48,11 @@ $(function() {
     // Link Highlighting
     if (pos2 > $('#home').offset().top) { highlightLink('home'); }
     if (pos2 > $('#about').offset().top) { highlightLink('about'); }
-    if (pos2 > $('#portfolio').offset().top) { highlightLink('portfolio'); }
+    if (pos2 > $('#sounds').offset().top) { highlightLink('sounds'); }
     if (pos2 > $('#writings').offset().top) { highlightLink('writings'); }
-    if (pos2 > $('#contact').offset().top ||
+    if (pos2 > $('#contacts').offset().top ||
       pos + $(window).height() === $(document).height()) {
-      highlightLink('contact');
+      highlightLink('contacts');
     }
 
     // Prevent Hover on Scroll
